34b4e1e6c6bcab5b547f8f1e98d3a6c1de26f138
zingsmack <orangetalisman@outlook.com>
2025-04-25 04:17:06 +0100

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -5,6 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
     { src: 'images/two.jpg',   caption: 'Portrait 2: [Your caption]' },
     { src: 'images/three.jpg', caption: 'Portrait 3: [Your caption]' },
     { src: 'images/four.jpg',  caption: 'Portrait 4: [Your caption]' },
+    { src: 'images/five.jpg',   caption: 'Portrait 5: [Your caption]' },
+    { src: 'images/six.jpg',   caption: 'Portrait 6: [Your caption]' },
+    { src: 'images/seven.jpg', caption: 'Portrait 7: [Your caption]' },
+    { src: 'images/eight.jpg',  caption: 'Portrait 8: [Your caption]' },   
+    { src: 'images/nine.jpg',   caption: 'Portrait 9: [Your caption]' },
+    { src: 'images/ten.jpg',   caption: 'Portrait 10: [Your caption]' },
+    { src: 'images/eleven.jpg', caption: 'Portrait 11: [Your caption]' },
+    { src: 'images/twelve.jpg',  caption: 'Portrait 12: [Your caption]' },
+    { src: 'images/thirteen.jpg',   caption: 'Portrait 13: [Your caption]' },
+    { src: 'images/fourteen.jpg',   caption: 'Portrait 14: [Your caption]' },
+    { src: 'images/fifteen.jpg', caption: 'Portrait 15: [Your caption]' },
+    { src: 'images/sixteen.jpg',  caption: 'Portrait 16: [Your caption]' },
+    { src: 'images/seventeen.jpg',   caption: 'Portrait 17: [Your caption]' },
+    { src: 'images/eighteen.jpg',   caption: 'Portrait 18: [Your caption]' },
+    { src: 'images/nineteen.jpg', caption: 'Portrait 19: [Your caption]' },
+
+
+    
   ];
   let current = 0;
 
